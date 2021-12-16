8429d07d3412b2343f6eea3be79fa49538474bd5
MD Suhaib Safwan <68965547+MdSuhaibSafwan@users.noreply.github.com>
2021-12-15 20:57:55 -0800

    initialized the project for developing APIs

---
+++ api/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Blog(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=250)
+    content = models.TextField()
+    date_created = models.DateTimeField(auto_now_add=True)
+    last_updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
